8fb2d4aaab14492b135e6f1313c5e731aaefe1ac
Dominic Wagas <dominicwagas@gmail.com>
2023-08-10 16:23:53 +0800

    fix front page

--- components/Main/AIForEveryone.js
+++ components/Main/AIForEveryone.js
@@ -2,7 +2,7 @@ import React from 'react'
 const AIForEveryone = () => {
     return (
         <section className='w-full'>
-            <div className="bg-white flex min-h-[55vh] flex-row justify-start items-center w-full">
+            <div className="bg-white flex min-h-[55vh]  xs:flex-col sm:flex-col md:flex-row lg:flex-row xs:justify-center sm:justify-center md:justify-start lg:justify-start items-center w-full">
                 <div className="flex flex-col justify-center w-[70%] m-auto  ">
                     <h3 className="text-black text-center text-5xl pt-[2rem] pr-0 pb-[0.5rem] pl-0 font-sans font-light">AI For Everyone</h3>
                     <h5 className="text-black text-center text-2xl pt-[2rem] pr-0 pb-[0.5rem] pl-0 font-sans  ">Get your team trained by Red Dragon AI, courses now available.</h5>
@@ -10,7 +10,7 @@ const AIForEveryone = () => {
                         <button className="bg-[#de0800] text-xl font-normal rounded-xl touch-manipulation border border-[#de0800] hover:bg-[#ff1b12] py-2 px-5 hover:animate-pulse">Training Now</button>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center flex-1 animate-pulse">
+                <div className="flex  justify-center flex-1 mx-auto animate-pulse xs:my-4 sm:my-4">
                     <img className="w-[65%] h-auto" src={"/img/home_page_imgs/developer_dl_series_img.png"} alt="Training Neurons" />
                 </div>
             </div>
--- components/Main/RDAISolutions.js
+++ components/Main/RDAISolutions.js
@@ -3,10 +3,10 @@ import { InView } from 'react-intersection-observer'
 const RDAISolutions = () => {
     return (
         <section>
-            <div className="bg-[#282727] flex min-h-[55vh] flex-row justify-start items-center">
+            <div className="bg-[#282727] flex min-h-[55vh] xs:flex-col sm:justify-center md:flex-row md:justify-start items-center">
                 <InView>
                     {({ inView, ref, entry }) => (
-                        <div ref={ref} className={"-translate-x-full transition-transform delay-300 duration-500 ease-out flex w-[35%] m-auto flex-1 " + (inView ? "translate-x-0" : "-translate-x-full")}>
+                        <div ref={ref} className={"-translate-x-full transition-transform delay-300 duration-500 ease-out flex xs:w-[80%] sm:w-[80%] md:w-[35%] lg:w-[35%] m-auto flex-1 " + (inView ? "translate-x-0" : "-translate-x-full")}>
                             <div className="flex flex-col justify-center items-center ">
                                 <h3 className='text-white text-center text-4xl pt-[2rem] pr-0 pb-[0.5rem] pl-0 font-sans font-light'>Red Dragon AI NLU <br></br> the Natural Language Understanding Platform</h3>
                                 <p className='w-[70%] text-center text-white text-xl pr-0 pb-[0.5rem] pl-0 font-serif'>Red Dragon AI uses multiple technologies to deliver ML architectures that accelerate your Natural Language Understanding and Processing tasks, in the cloud, on device and hybrid systems. Learn more in our solution guide.</p>
